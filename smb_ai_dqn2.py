e5f4824ba6bb2eb0c7647e183361ba44cc44b391
hatanuk <110492450+hatanuk@users.noreply.github.com>
2024-12-02 18:36:58 +0100

    render mode to none

--- smb_ai_dqn2.py
+++ smb_ai_dqn2.py
@@ -513,7 +513,7 @@ class MainWindow(QtWidgets.QMainWindow):
 
 def run_ga_agent(config, data_queue):
     # Initialize environment
-    env = retro.make(game='SuperMarioBros-Nes', state=f'Level{config.Misc.level}')
+    env = retro.make(game='SuperMarioBros-Nes', state=f'Level{config.Misc.level}', render_mode=None)
 
     # Initialize population and agent
     individuals = _initialize_population(config)
@@ -651,7 +651,7 @@ def run_ga_agent(config, data_queue):
 
 def run_dqn_agent(config, data_queue):
     # Initialize environment
-    env = retro.make(game='SuperMarioBros-Nes', state=f'Level{config.Misc.level}')
+    env = retro.make(game='SuperMarioBros-Nes', state=f'Level{config.Misc.level}', render_mode=None)
 
     # Initialize DQN agent
     mario_DQN = DQNMario(config)
