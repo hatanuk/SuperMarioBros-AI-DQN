495f39eb2e720bfc6297d10c9d725d250ad38f96
hatanuk <110492450+hatanuk@users.noreply.github.com>
2024-12-02 18:45:18 +0100

    again

--- smb_ai_dqn2.py
+++ smb_ai_dqn2.py
@@ -410,7 +410,7 @@ class MainWindow(QtWidgets.QMainWindow):
             NeuralNetworkViz(
                 self.centralWidget,
                 None,
-                (256, 224),
+                (100, 80),
                 self.config,
                 nn_params=self.config.NeuralNetworkGA
             )
@@ -427,7 +427,7 @@ class MainWindow(QtWidgets.QMainWindow):
             NeuralNetworkViz(
                 self.centralWidget,
                 None,
-                (256, 224),
+                (100, 80),
                 self.config,
                 nn_params=self.config.NeuralNetworkDQN
             )
