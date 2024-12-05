834e561130f3baa10347f0112635dfef1bd5e087
hatanuk <110492450+hatanuk@users.noreply.github.com>
2024-12-05 23:07:13 +0100

    .

--- DQN_algorithm/DQNbaseline.py
+++ DQN_algorithm/DQNbaseline.py
@@ -247,7 +247,7 @@ class DQNMario(Mario):
         self.train_freq = self.config.DQN.train_freq
 
         # specifies the model architecture for the DQN
-        policy_kwargs = dict(activation_fn=get_torch_activation_by_name(self.hidden_activation), net_arch=self.hidden_layer_architecture)
+        policy_kwargs = dict(activation_fn=get_torch_activation_by_name(self.hidden_activation), net_arch=self.hidden_layer_architecture, output_a=get_torch_activation_by_name(self.output_activation))
 
         self.model = ModifiedDQN('MlpPolicy', 
                     env=env, 
