9e6d227fc261721b96b055bec0b35cfc943f5af2
hatanuk <110492450+hatanuk@users.noreply.github.com>
2024-12-10 22:00:32 +0100

    .

--- smb_ai_dqn4.py
+++ smb_ai_dqn4.py
@@ -432,7 +432,7 @@ if __name__ == "__main__":
             "max_fitness": 0,
             "max_distance": 0,
             "current_ind": 0,
-            "action_counts" = [0] * 5
+            "action_counts": [0] * 5
 
         }
 
