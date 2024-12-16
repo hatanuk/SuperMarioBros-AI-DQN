32b74d16edbd8af07a4fa45d12b5ad6b80af5a70
hatanuk <110492450+hatanuk@users.noreply.github.com>
2024-12-16 22:17:50 +0100

    .

--- get_info.py
+++ get_info.py
@@ -9,7 +9,7 @@ if __name__ == "__main__":
 
     #print(f"Algorithm: {info['algorithm']}")
     print(f"Distance achieved: {info['distance']}")
-    print(f"Number of iterations: {info['num_iterations']}")
+    print(f"Number of iterations: {info['iterations']}")
     print(f"Output activation: {info['output_activation']}")
     print(f"Hidden activation: {info['hidden_activation']}")
     print(f"Architecture: {info['architecture']}")
