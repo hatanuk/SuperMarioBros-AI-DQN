ef3bdb428998396c2e5d8b2553c19b3df0a2d1c5
hatanuk <110492450+hatanuk@users.noreply.github.com>
2024-12-16 22:29:09 +0100

    .

--- run_model.py
+++ run_model.py
@@ -15,7 +15,7 @@ from DQN_algorithm.DQNbaseline import InputSpaceReduction
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument('--model_path', type=str, required=True, help='Path to saved Mario .pt model')
+    parser.add_argument('model_path', type=str, help='Path to saved Mario .pt model')
     parser.add_argument('--level', type=str, default="1-1", help='Which level to load')
     parser.add_argument('--config', type=str, default='settings.config', help='Path to config')
     parser.add_argument('--arch', type=str, default='ga', help='Whether to use the DQN or GA NN architecture defined in config. Only matter if they differ.')
