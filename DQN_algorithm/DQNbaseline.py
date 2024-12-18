83c2b0143b79cd68a849390d390c245b1f17ad6d
hatanuk <110492450+hatanuk@users.noreply.github.com>
2024-12-18 23:25:17 +0100

    .

--- DQN_algorithm/DQNbaseline.py
+++ DQN_algorithm/DQNbaseline.py
@@ -252,6 +252,8 @@ class DQNCallback(BaseCallback):
 
     def _on_step(self) -> bool:
 
+
+
         done = True if self.locals['dones'].any() else False
 
         actions = self.locals['actions']
@@ -273,6 +275,7 @@ class DQNCallback(BaseCallback):
             self.best_model_state_dict = copy.deepcopy(self.model.policy.state_dict())
 
         if done:
+            print(f"Step: {self.num_timesteps}, Loss: {self.model.policy.loss}")
             # manually update epsilon
             self.model.exploration_rate = self.epsilon_scheduler.get_epsilon(self.episode)
 
